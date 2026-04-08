68edda0dfaaf2c251633711dc49906688b48ab26
SamJDriver <samjdriv@gmail.com>
2026-04-08 00:48:52 -0700

    initial commit. Set up mod entry with assets

---
+++ IndustrialFurnace/ModConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace IndustrialFurnace;
+
+public class ModConfig
+{
+    public int CoalAmount { get; set; } = 5;
+    public bool InstantSmelting { get; set; } = false;
+    public bool EnableSmokeAnimation { get; set; } = true;
+    public bool EnableFireAnimation { get; set; } = true;
+}
+
